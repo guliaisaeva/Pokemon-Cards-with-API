a5a56c4989c490a7f8f57460e119dfb7162af1f2
Gulia <gulia.isaeva.pls@gmail.com>
2022-11-03 20:47:30 +0300

    add js file and start to add js variables

---
+++ script.js
@@ -0,0 +1,21 @@
+const poke_container=document.getElementById('pokemon-container')
+const pokemon_count=150
+const colors={
+    fire:"#FDDFDF",
+    grass:"#DEFDE0",
+    electric:"#FCF7DE",
+    water:"#DEF#FD",
+    ground:"#f4e7da",
+    rock:"#d5d5d4",
+    fairy:"#fceaff",
+    poison:"#98d7a5",
+    bug:"#f8d5a3",
+    dragon:"#97b3e6",
+    psychic:"#eaeda1",
+    flying:"#f5f5f5",
+    fighting:"#E6E0D4",
+    normal:"#F5F5F5"
+}
+const fetchPokemons=()=>{
+
+}
